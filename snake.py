598a3e600978a73cf1a1ea1dded64753f6f0d83c
jakecard20 <jakecard20@gmail.com>
2021-05-17 01:38:55 -0400

    Added classes, can now replay the game, added curses wrapper so terminal doesnt soft lock

--- snake.py
+++ snake.py
@@ -1,66 +1,106 @@
 import random
 import curses
 
-shell = curses.initscr()
-curses.curs_set(0)
-shell_height, shell_width = shell.getmaxyx()
+class snakeGame:
 
-window = curses.newwin(shell_height, shell_width, 0, 0)
+	def __init__(self, height, width):
+		self.shell_h = height 
+		self.shell_w = width
 
-window.keypad(1) 	#Accpet keypad inputs
+		if(height < 30 or width < 60):
+			raise ValueError("Minimum shell size requirements not met")
+		self.snk_x = int(width / 3)
+		self.snk_y = int(height / 2) 
+		self.score = 0
+		self.snake = [[self.snk_y, self.snk_x], [self.snk_y, self.snk_x-1], [self.snk_y, self.snk_x-2]]
+		self.food = [int(width / 2), int(height/2)]
 
-window.timeout(100) #Set refresh rate
+	def initWindow(self):
+		curses.initscr()
+		curses.curs_set(0)
+		window = curses.newwin(self.shell_h, self.shell_w, 0, 0)
+		window.keypad(1) 	#Accpet keypad inputs
+		window.timeout(100) #Set refresh rate
+
+		#Reset game variables
+		self.snk_x = int(self.shell_w / 3)
+		self.snk_y = int(self.shell_h / 2) 
+		self.score = 0
+		self.snake = [[self.snk_y, self.snk_x], [self.snk_y, self.snk_x-1], [self.snk_y, self.snk_x-2]]
+		self.food = [int(self.shell_h / 2), int(self.shell_w/2)]
 
 
-snk_x = shell_width/4
-snk_y = shell_height/2
-snake = [
-	[snk_y, snk_x],
-	[snk_y, snk_x - 1],
-	[snk_y, snk_x - 2]
-	]
-print(snake)
-food = [shell_height/2, shell_width/2]
-print(int(food[0]))
-print(int(food[1]))
-print(curses.ACS_PI)
-window.addch(int(food[0]), int(food[1]), '@')
-#window.addch(curses.ACS_PI)
+		window.addch(int(self.food[0]), int(self.food[1]), curses.ACS_PI)
 
-key = curses.KEY_RIGHT
+		return window
 
-while True:
-	next_key = window.getch()
-	key = key if next_key == -1 else next_key
+	def playGame(self):
+		window = self.initWindow()
+		key = curses.KEY_RIGHT
 
-	if snake[0][0] in [0, shell_height] or snake[0][1] in [0, shell_width] or snake[0] in snake[1:]:
-		curses.endwin()
-		quit()
+		while True:
+			next_key = window.getch()
+			key = key if next_key == -1 else next_key
 
-	new_head = [snake[0][0], snake[0][1]]
+			# Check if snake is out of bounds or has consumed itself
+			if (self.snake[0][0] in [0, self.shell_h] 
+				or self.snake[0][1] in [0, self.shell_w] 
+				or self.snake[0] in self.snake[1:]):
+				window.addstr(int(self.shell_h/8), int(self.shell_w/2), "Game Over Score:{0}".format(self.score))
+				window.addstr(int(self.shell_h/8 + 1), int(self.shell_w/2), "Would you like to play again?")
+				window.addstr(int(self.shell_h/8 + 2), int(self.shell_w/2), "y/n")
+				while True:
+					next_key = window.getch()
+					key = key if next_key == -1 else next_key
+					if key == ord('y'):
+						window.clear()
+						window = self.initWindow()
+						key = curses.KEY_RIGHT
+						break
+					if key == ord('n'):
+						curses.endwin()
+						quit()
 
-	if key == curses.KEY_DOWN:
-		new_head[0] += 1
-	if key == curses.KEY_UP:
-		new_head[0] -= 1
-	if key == curses.KEY_LEFT:
-		new_head[1] -= 1
-	if key == curses.KEY_RIGHT:
-		new_head[1] += 1
+			new_head = [self.snake[0][0], self.snake[0][1]]
 
-	snake.insert(0, new_head)
+			# Keys pertaining to movement
+			if key == curses.KEY_DOWN:
+				new_head[0] += 1
+			if key == curses.KEY_UP:
+				new_head[0] -= 1
+			if key == curses.KEY_LEFT:
+				new_head[1] -= 1
+			if key == curses.KEY_RIGHT:
+				new_head[1] += 1
+			# 'q' to quit
+			if key == ord('q'):
+				curses.endwin
+				quit()
 
-	if snake[0] == food:
-		food = None
-		while food is None:
-			newFood = [
-				random.randint(1, shell_height - 1),
-				random.randint(1, shell_width - 1)
-			]
-			food = newFood if newFood not in snake else None
-			window.addch(int(food[0]), int(food[1]), curses.ACS_PI)
-	else:
-		tail = snake.pop()
-		window.addch(int(tail[0]), int(tail[1]), ' ')
+			self.snake.insert(0, new_head)
 
-	window.addch(int(snake[0][0]), int(snake[0][1]), curses.ACS_CKBOARD)
+			if self.snake[0] == self.food:
+				self.score += 1
+				self.food = None
+				while self.food is None:
+					newFood = [
+						random.randint(1, self.shell_h - 1),
+						random.randint(1, self.shell_w - 1)
+					]
+					self.food = newFood if newFood not in self.snake else None
+					#Add food character to the window
+					window.addch(int(self.food[0]), int(self.food[1]), curses.ACS_PI)
+			else:
+				tail = self.snake.pop()
+				window.addch(int(tail[0]), int(tail[1]), ' ')
+
+			window.addch(int(self.snake[0][0]), int(self.snake[0][1]), curses.ACS_CKBOARD)
+
+def main(stdscr):
+	max_y, max_x = stdscr.getmaxyx()
+	print (max_y)
+	print (max_x)
+	game = snakeGame(max_y, max_x)
+	game.playGame()
+
+curses.wrapper(main)
